cca88765ee7e0a23c545e70657e794319a1c335b
Demo-Kim <76992124+Demo-Kim@users.noreply.github.com>
2021-05-15 00:07:49 +0900

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+package NL_02;
+
+public class test {
+	public static void main(String[] args) {
+		Car car1 = new Car("¶Ù¶Ù", 60, 0);	
+		Car car2 = new Car("»§»§", 40, 0);
+		System.out.println(car1.toString());
+		System.out.println(car2.toString());
+		car1.Drive(3);
+		car2.Drive(5);
+	}
+}
