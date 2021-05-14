26f10397a35b9304e5590c0023847d5385797974
Demo-Kim <76992124+Demo-Kim@users.noreply.github.com>
2021-05-15 00:10:36 +0900

    Update test.java

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ package NL_02;
 
 public class test {
 	public static void main(String[] args) {
-		Car car1 = new Car("뛰뒤", 60, 0);	
+		Car car1 = new Car("뛰뛰", 60, 0);	
 		Car car2 = new Car("빵빵", 40, 0);
 		System.out.println(car1.toString());
 		System.out.println(car2.toString());
